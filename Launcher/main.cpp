adac5ed21e67170d5b09d20609b7799122ac01d8
Antoine Lafarge <ant.lafarge@gmail.com>
2017-07-28 16:43:07 +0200

    update example

--- Launcher/main.cpp
+++ Launcher/main.cpp
@@ -5,12 +5,13 @@
 
 int main()
 {
+	/*
 	auto i = new int(13);
 	delete i;
-
+	*/
 	auto j = new int[12];
 	delete[] j;
-
+	/*
 	auto m = new Machin();
 	m->test();
 	delete m;
@@ -19,7 +20,7 @@ int main()
 	ss << 13;
 	ss << 37;
 	std::string str = ss.str();
-	
+	*/
 	std::cin.ignore();
 
 	return 0;
