e7481a0a6c5927a4b310018a23bba5c63d3f0a71
Antoine Lafarge <ant.lafarge@gmail.com>
2017-07-31 12:32:54 +0200

    - simplify example

--- Launcher/main.cpp
+++ Launcher/main.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
-#include "Bidule.h"
 #include <sstream>
 
 int main()
 {
-	//auto i = new int(13);
-	//delete i;
+	auto i = new int(13);
+	delete i;
 	
 	auto j = new int[10];
 	delete[] j;
 	
-	/*
-	auto c = new Bidule();
-	auto h = c->create();
-	//delete h;
-	c->destroy(h);
-	delete c;
-	*/
-
 	std::cin.ignore();
 
 	return 0;
--- Lib/New.cpp
+++ Lib/New.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 #include <cstdlib>
 
-void* operator new(size_t s)
+inline void* operator new(size_t s)
 {
 	auto ptr = _malloc_dbg(s, _NORMAL_BLOCK, NULL, NULL);
 	std::cout << "new " << (uintptr_t)ptr << " (" << s << " bytes )" << std::endl;
 	return ptr;
 }
 
-void operator delete(void* ptr)
+inline void operator delete(void* ptr)
 {
 	std::cout << "delete " << (uintptr_t)ptr << std::endl;
 	_free_dbg(ptr, _NORMAL_BLOCK);
 }
-/*
-void* operator new[](size_t s)
+
+inline void* operator new[](size_t s)
 {
 	auto ptr = _malloc_dbg(s, _NORMAL_BLOCK, NULL, NULL);
 	std::cout << "new " << (uintptr_t)ptr << " (" << s << " bytes )" << std::endl;
 	return ptr;
 }
 
-void operator delete[](void* ptr)
+inline void operator delete[](void* ptr)
 {
 	std::cout << "delete " << (uintptr_t)ptr << std::endl;
 	_free_dbg(ptr, _NORMAL_BLOCK);
 }
-*/
